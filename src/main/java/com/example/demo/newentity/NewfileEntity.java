7aef86585ba5e1b1e6708375d7d468c309647da2
bala-amypo <bala@amypo.in>
2025-12-17 08:11:14 +0000

    User commit

---
+++ src/main/java/com/example/demo/newentity/NewfileEntity.java
@@ -0,0 +1,11 @@
+package com.example.demo.newentity;
+
+import jakarta.persistence.id;
+import jakarta.validation.constraints.NotBlank;
+
+public class NewfileEntity{
+    @Id
+    private Long id;
+    @NotBlank
+    private Strin
+}
