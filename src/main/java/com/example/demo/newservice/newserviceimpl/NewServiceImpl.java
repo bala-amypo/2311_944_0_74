82988623b3edcdd139de9290b6f9361d66050e90
bala-amypo <bala@amypo.in>
2025-12-17 09:31:12 +0000

    User commit

---
+++ src/main/java/com/example/demo/newservice/newserviceimpl/NewServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.demo.newservice.newserviceimpl;
+
+import java.util.List;
+import org.springframework.steretype.Service;
+import com.example.demo.newservice.NewfileService;
+
+@Seriv
